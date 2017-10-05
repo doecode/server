7f6cc388392270c981f922b5dda5ecd9f9e6171d
sowerst <sowerst@osti.gov>
2017-10-05 16:50:40 -0400

    Remove URL from repo link error message; (DOECODE-146)

--- src/main/java/gov/osti/services/Validation.java
+++ src/main/java/gov/osti/services/Validation.java
@@ -40,9 +40,9 @@ import org.slf4j.LoggerFactory;
 
 /**
  * Private Class for Validation Responses.
- * 
+ *
  * Must be non-inner-class for Jackson purposes.
- * 
+ *
  * @author ensornl
  */
 class ApiResponse {
@@ -52,7 +52,7 @@ class ApiResponse {
     public ApiResponse() {
 
     }
-    
+
     /**
      * @return the isValid
      */
@@ -85,7 +85,7 @@ class ApiResponse {
 /**
  *
  * REST web services for validation purposes.
- * 
+ *
  * @author ensornl
  */
 @Path("validation")
@@ -102,14 +102,14 @@ public class Validation {
     private static final ObjectMapper mapper = new ObjectMapper();
     // static DOI resolution prefix
     private static final String DOI_BASE_URL = "https://doi.org/";
-    
+
     // Phone number validation
     private static final PhoneNumberUtil PHONE_NUMBER_VALIDATOR = PhoneNumberUtil.getInstance();
     // regular expressions for validating email addresses and URLs
     protected static final Pattern EMAIL_PATTERN = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
     protected static final Pattern URL_PATTERN = Pattern.compile("\\bhttps?://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]");
     protected static final Pattern DOI_PATTERN = Pattern.compile("10.\\d{4,9}/[-._;()/:A-Za-z0-9]+$");
-    
+
     @JsonIgnoreProperties (ignoreUnknown = true)
     private static class ValidationRequest implements Serializable {
         private String type;
@@ -158,16 +158,16 @@ public class Validation {
             this.error = error;
         }
     }
-    
+
     /**
      * Creates a new instance of ValidationResource
      */
     public Validation() {
     }
-    
+
     /**
      * Determine whether or not this VALUE conforms to an EMAIL ADDRESS pattern.
-     * 
+     *
      * @param value the value to check
      * @return true if matches an EMAIL ADDRESS pattern, false if not
      */
@@ -176,12 +176,12 @@ public class Validation {
                 false :
                 EMAIL_PATTERN.matcher(value).matches();
     }
-    
+
     /**
      * Check to see if a VALUE appears to be a valid URL.
-     * 
+     *
      * If no "http" prefix found, add one and try that value.
-     * 
+     *
      * @param value the VALUE to check
      * @return true if appears to be a URL, false if not
      */
@@ -192,10 +192,10 @@ public class Validation {
                 URL_PATTERN.matcher(value).matches() :
                 URL_PATTERN.matcher("http://"+value).matches();
     }
-    
+
     /**
      * Determine whether or not the phone number is valid, defaulting to US.
-     * 
+     *
      * @param value the PHONE NUMBER
      * @return true if a valid number, false if not
      */
@@ -210,12 +210,12 @@ public class Validation {
         }
         return false;
     }
-    
+
     /**
      * Make an external validation call for a CONTRACT NUMBER for validity.
      * If unable to check, not configured properly, or an error occurs, assume
      * FALSE.
-     * 
+     *
      * @param value the CONTRACT/AWARD NUMBER to check
      * @return true if valid, false if not
      */
@@ -230,20 +230,20 @@ public class Validation {
                 .create()
                 .setDefaultRequestConfig(rc)
                 .build();
-        
+
         try {
             // if not configured, abort
             if (StringUtils.isBlank(API_HOST))
                 return false;
-            
+
             // call the VALIDATION API to get a response
             HttpGet get = new HttpGet(API_HOST + "/contract/validate/" + URLEncoder.encode(value.trim(), "UTF-8"));
             HttpResponse response = hc.execute(get);
             // get the RESPONSE
             ApiResponse apiResponse = mapper.readValue(response.getEntity().getContent(), ApiResponse.class);
-            
+
             return apiResponse.isValid();
-        } catch ( IOException e ) { 
+        } catch ( IOException e ) {
             log.warn("Error checking " + value + ": " + e.getMessage());
         } finally {
             try {
@@ -254,23 +254,23 @@ public class Validation {
         }
         return false;
     }
-    
+
     /**
      * Determine whether or not the passed-in value is a VALID repository link.
-     * 
+     *
      * Presently, valid means a remote-accessible HTTP(S)-based git repository.
-     * 
+     *
      * @param value the repository link/URL to check
      * @return true if valid, false if not
      */
     public static boolean isValidRepositoryLink(String value) {
         if ( StringUtils.isBlank(value))
             return false;
-        
+
         // if not starting with HTTP, make it so then test
         if (!value.toLowerCase().startsWith("http"))
             value = "https://" + value;
-        
+
         try {
             Collection<Ref> references = Git
                     .lsRemoteRepository()
@@ -278,7 +278,7 @@ public class Validation {
                     .setTags(true)
                     .setRemote(value)
                     .call();
-            
+
             // must be a valid repository if it has references
             return true;
         } catch ( Exception e ) {
@@ -287,11 +287,11 @@ public class Validation {
             return false;
         }
     }
-    
+
     /**
      * Check to see if DOI is valid or not.  Matches the pattern of a DOI URL, or
      * bare DOI value.
-     * 
+     *
      * @param value the DOI to check
      * @return true if the DOI is a valid pattern; false if not
      */
@@ -299,10 +299,10 @@ public class Validation {
         // make sure the value, if present, conforms to DOI pattern.
         return (null==value) ? false : DOI_PATTERN.matcher(value).find();
     }
-    
+
     /**
      * View the API documentation.
-     * 
+     *
      * @return a Viewable to the documentation
      */
     @GET
@@ -310,14 +310,14 @@ public class Validation {
     public Viewable getDocumentation() {
         return new Viewable ("/validation");
     }
-    
+
     /**
      * Determine whether or not the PHONE NUMBER is valid.
-     * 
-     * Response Codes: 
+     *
+     * Response Codes:
      * 200 - OK, value is valid
      * 400 - Bad Request, value is NOT valid
-     * 
+     *
      * @param value a PHONE NUMBER to check
      * @return a Response containing validity
      */
@@ -329,14 +329,14 @@ public class Validation {
                 Response.ok().entity(mapper.createObjectNode().put("value", "OK").toString()).build() :
                 ErrorResponse.badRequest("\"" + value + "\" is not a valid phone number.").build();
     }
-    
+
     /**
      * Check an AwardNumber for valid DOE contract number value.
-     * 
-     * Response Codes: 
+     *
+     * Response Codes:
      * 200 - OK, value is valid
      * 400 - Bad Request, value is NOT valid
-     * 
+     *
      * @param value the AWARD NUMBER
      * @return a Response containing validity
      */
@@ -348,14 +348,14 @@ public class Validation {
                 Response.ok().entity(mapper.createObjectNode().put("value", "OK").toString()).build() :
                 ErrorResponse.badRequest("\"" + value + "\" is not a valid award number.").build();
     }
-    
+
     /**
      * Check a DOI.
-     * 
-     * Response Codes: 
+     *
+     * Response Codes:
      * 200 - OK, value is valid
      * 400 - Bad Request, value is NOT valid
-     * 
+     *
      * @param value the DOI to check
      * @return a Response
      */
@@ -367,14 +367,14 @@ public class Validation {
                 Response.ok().entity(mapper.createObjectNode().put("value", "OK").toString()).build() :
                 ErrorResponse.badRequest("\"" + value + "\" is not a valid DOI.").build();
     }
-    
+
     /**
      * Check an email address.
-     * 
-     * Response Codes: 
+     *
+     * Response Codes:
      * 200 - OK, value is valid
      * 400 - Bad Request, value is NOT valid
-     * 
+     *
      * @param value the email address to check
      * @return a Response
      */
@@ -386,14 +386,14 @@ public class Validation {
                 Response.ok().entity(mapper.createObjectNode().put("value", "OK").toString()).build() :
                 ErrorResponse.badRequest("\"" + value + "\" is not a valid email address.").build();
     }
-    
+
     /**
      * Check a REPOSITORY LINK value.
-     * 
-     * Response Codes: 
+     *
+     * Response Codes:
      * 200 - OK, value is valid
      * 400 - Bad Request, value is NOT valid
-     * 
+     *
      * @param value a REPOSITORY LINK to check
      * @return a Response containing whether or not this was valid
      */
@@ -403,16 +403,16 @@ public class Validation {
     public Response checkRepositoryLink(@QueryParam("value") String value) {
         return ( isValidRepositoryLink(value) ) ?
                 Response.ok().entity(mapper.createObjectNode().put("value", "OK").toString()).build() :
-                ErrorResponse.badRequest("\"" + value + "\" is not a valid repository link.").build();
+                ErrorResponse.badRequest("Not a valid repository link.").build();
     }
-    
+
     /**
      * Check a URL value.
-     * 
-     * Response Codes: 
+     *
+     * Response Codes:
      * 200 - OK, value is valid
      * 400 - Bad Request, value is NOT valid
-     * 
+     *
      * @param value a URL value to check
      * @return a Response containing validity
      */
@@ -427,24 +427,24 @@ public class Validation {
 
     /**
      * Determine whether or not a contract number is valid.
-     * 
-     * Receive JSON: 
+     *
+     * Receive JSON:
      * [ { "value":"value1", "type":"type1" }, {"value":"value2", "type":"type2"} ]
-     * 
+     *
      * Return:
-     * 
+     *
      * [ { "value":"value1", "type":"type1", "error":"Not a valid type1"}, ...]
-     * 
+     *
      * Empty "error messages" implies value was accepted and passes indicated validation
-     * rule(s). 
-     * 
+     * rule(s).
+     *
      * Types supported (NOT case-sensitive): "doi", "awardnumber", "phonenumber",
      * "email", "url", "repositorylink".
-     * 
+     *
      * @param object the String containing JSON of the validation request
-     * 
+     *
      * @return JSON containing records with "error" messages with each
-     * 
+     *
      * @throws java.io.IOException on IO or HTTP client errors
      */
     @POST
@@ -453,19 +453,19 @@ public class Validation {
     public Response request(String object) throws IOException {
         try {
             ValidationRequest[] requests = mapper.readValue(object, ValidationRequest[].class);
-            
+
             /**
              * Validations:
-             * 
+             *
              * "DOI" -- ensure that DOI_BASE_URL + value is reachable via the internets
              * "Award" -- call known validation endpoint with value, check for "isValid" true response
-             * 
+             *
              */
             for ( ValidationRequest req : requests ) {
                 if (StringUtils.equalsIgnoreCase(req.getType(), "doi")) {
                     req.setError((isValidDoi(req.getValue()) ? "" : req.getValue() + " is not a valid DOI."));
                 } else if (StringUtils.equalsIgnoreCase(req.getType(), "repositorylink")) {
-                    req.setError((isValidRepositoryLink(req.getValue()) ? "" : req.getValue() + " is not a valid repository link."));
+                    req.setError((isValidRepositoryLink(req.getValue()) ? "Not a valid repository link."));
                 } else if (StringUtils.equalsIgnoreCase(req.getType(), "phonenumber")) {
                     req.setError((isValidPhoneNumber(req.getValue()) ? "" : req.getValue() + " is not a valid phone number."));
                 } else if (StringUtils.equalsIgnoreCase(req.getType(), "url")) {
