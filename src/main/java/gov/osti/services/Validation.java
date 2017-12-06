122e4483cc3be0812ffc84b9dcdbc38cb5d725f6
sowerst <sowerst@osti.gov>
2017-12-06 09:52:40 -0500

    Alter message returned for URLs missing a protocol.

--- src/main/java/gov/osti/services/Validation.java
+++ src/main/java/gov/osti/services/Validation.java
@@ -496,7 +496,7 @@ public class Validation {
          String msg = "";
 
          if (!safeUrl.startsWith("http"))
-              msg = "This URL is missing a valid protocol.  Please prefix it with \"http://\" or \"https://\" as appropriate.";
+              msg = "Please include a correclty associated prefix with the URL (e.g., \"http://\" or \"https://\").";
          else {
               if (type.equalsIgnoreCase("repositorylink"))
                    msg = "Not a valid repository link.";
